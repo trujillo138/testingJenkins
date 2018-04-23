3df3c40afdbb2b797e94a6c508f910831739215a
Tomas Trujillo <tomastrujillo@Tomass-MacBook-Pro.local>
2018-04-23 15:22:22 -0500

    Adding Xcode project

---
+++ JenkinsProject/JenkinsProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JenkinsProject
+//
+//  Created by Tomas Trujillo on 4/23/18.
+//  Copyright © 2018 Tomas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JenkinsProject/JenkinsProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JenkinsProject
+//
+//  Created by Tomas Trujillo on 4/23/18.
+//  Copyright © 2018 Tomas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
